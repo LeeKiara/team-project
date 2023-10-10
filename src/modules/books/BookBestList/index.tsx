afa98348b09b5b5375d912ac6a2416c3c0530f8e
Aejin Kang <dowlsdl003@naver.com>
2023-10-10 12:35:52 +0900

    db.json 추가

---
+++ src/modules/books/BookBestList/index.tsx
@@ -0,0 +1,83 @@
+import { BookBestContainer } from "./styles";
+
+const BookBestList = () => {
+  return (
+    <>
+      <BookBestContainer>
+        <section>
+          <h2>베스트 셀러</h2>
+          <article>
+            <div>
+              <figure>
+                <img src="책이미지" alt="책이미지" />
+              </figure>
+              <div>
+                <h3>책 제목</h3>
+                <p>책 저자</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Dolorum animi cum nisi illo vitae tenetur ut? Veniam,
+                  repudiandae culpa libero ea, commodi natus totam, ducimus
+                  voluptas earum debitis dicta vel.
+                </p>
+              </div>
+            </div>
+            <ul>
+              <del>
+                <li>정가</li>
+              </del>
+              <li>판매가</li>
+              <li>장바구니 담기</li>
+            </ul>
+          </article>
+          <article>
+            <div>
+              <figure>
+                <img src="책이미지" alt="책이미지" />
+              </figure>
+              <div>
+                <h3>책 제목</h3>
+                <p>책 저자</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Dolorum animi cum nisi illo vitae tenetur ut? Veniam,
+                  repudiandae culpa libero ea, commodi natus totam, ducimus
+                  voluptas earum debitis dicta vel.
+                </p>
+              </div>
+            </div>
+            <ul>
+              <li>정가</li>
+              <li>판매가</li>
+              <li>장바구니 담기</li>
+            </ul>
+          </article>
+          <article>
+            <div>
+              <figure>
+                <img src="책이미지" alt="책이미지" />
+              </figure>
+              <div>
+                <h3>책 제목</h3>
+                <p>책 저자</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Dolorum animi cum nisi illo vitae tenetur ut? Veniam,
+                  repudiandae culpa libero ea, commodi natus totam, ducimus
+                  voluptas earum debitis dicta vel.
+                </p>
+              </div>
+            </div>
+            <ul>
+              <li>정가</li>
+              <li>판매가</li>
+              <li>장바구니 담기</li>
+            </ul>
+          </article>
+        </section>
+      </BookBestContainer>
+    </>
+  );
+};
+
+export default BookBestList;
