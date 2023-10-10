83cdcee4d401d727ac297b2accd740b5239b18a9
Aejin Kang <dowlsdl003@naver.com>
2023-10-10 19:30:45 +0900

    books 데이터 작성중

--- src/modules/books/BookBestList/index.tsx
+++ src/modules/books/BookBestList/index.tsx
@@ -15,6 +15,7 @@ const BookBestList = () => {
     <>
       <BookBestContainer>
         <section>
+          <div></div>
           <h2>베스트 셀러</h2>
           <p>{searchQuery}</p>
           <article>
@@ -81,33 +82,26 @@ const BookBestList = () => {
               <li>장바구니 담기</li>
             </ul>
           </article>
-          <article>
-            <div>
-              <figure>
-                <Link to="/page">
-                  <img
-                    src="https://www.cyber.co.kr/book/uploads/cache/2022-godo-goods/thumb-155247724937l0_300x0.jpg"
-                    alt="책이미지"
-                  />
-                </Link>
-              </figure>
-              <div>
-                <h3>책 제목</h3>
-                <p>책 저자</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Dolorum animi cum nisi illo vitae tenetur ut? Veniam,
-                  repudiandae culpa libero ea, commodi natus totam, ducimus
-                  voluptas earum debitis dicta vel.
-                </p>
-              </div>
-            </div>
-            <ul>
-              <li>정가</li>
-              <li>판매가</li>
-              <li>장바구니 담기</li>
-            </ul>
-          </article>
+          <nav style={{ display: "flex", justifyContent: "center" }}>
+            <ol style={{ display: "flex" }}>
+              <li className="numberbox">
+                <Link to="/">1</Link>
+              </li>
+              <li className="numberbox">
+                <Link to="/">2</Link>
+              </li>
+              <li className="numberbox">
+                <Link to="/">3</Link>
+              </li>
+              <li className="numberbox">
+                <Link to="/">4</Link>
+              </li>
+              <li className="numberbox">
+                <Link to="/">5</Link>
+              </li>
+              <li className="numberbox">{`>`}</li>
+            </ol>
+          </nav>
         </section>
       </BookBestContainer>
     </>
--- src/modules/books/data.ts
+++ src/modules/books/data.ts
@@ -1,4 +1,11 @@
-interface BookData {
+import axios from "axios";
+import useSWR from "swr";
+
+const booksApi = axios.create({
+  baseURL: "http://localhost:9090",
+});
+
+export interface BookData {
   id?: number;
   cover: string;
   title: string;
@@ -11,7 +18,7 @@ interface BookData {
   isbn: string;
 }
 
-interface BestBookData {
+export interface BestBookData {
   id?: number;
   cover: string;
   title: string;
@@ -26,4 +33,26 @@ interface BestBookData {
 
 const INIT_DATA: BookData[] = [];
 
-export const BOOKS_DATA_KEY = "/mall/books";
+export const BOOKS_DATA_KEY = "/books";
+
+const bookFetcher = async ([key, page]: string | number[]) => {
+  try {
+    const response = await booksApi.get<BookData[]>(
+      `${key}?_sort=id&_order=desc`
+    );
+    return response.data;
+  } catch (e: any) {
+    return INIT_DATA;
+  }
+};
+
+export const useBooksData = (page: number) => {
+  const {
+    data: booksData,
+    mutate,
+    isValidating: isBookDataValidating,
+  } = useSWR<BookData[]>([BOOKS_DATA_KEY, page], bookFetcher, {
+    fallbackData: INIT_DATA,
+    revalidateOnFocuse: false,
+  });
+};
