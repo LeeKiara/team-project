2b99f8a4f30ffde9decc71eb4a69dfd99f9b5f04
Aejin Kang <dowlsdl003@naver.com>
2023-10-10 19:50:30 +0900

    swr 설치

--- src/modules/books/data.ts
+++ src/modules/books/data.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 const booksApi = axios.create({
   baseURL: "http://localhost:9090",
@@ -53,6 +53,22 @@ export const useBooksData = (page: number) => {
     isValidating: isBookDataValidating,
   } = useSWR<BookData[]>([BOOKS_DATA_KEY, page], bookFetcher, {
     fallbackData: INIT_DATA,
-    revalidateOnFocuse: false,
+    revalidateOnFocus: false,
   });
 };
+
+function createBookData(book: BookData) {
+  mutate(async (preData: BookData[] = [...INIT_DATA]) => {
+    let nextData = [...preData];
+
+    try {
+      const response = await booksApi.post(BOOKS_DATA_KEY, book, {});
+      if (response.status === 201) {
+        nextData.unshift({ ...response.data });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+    return nextData;
+  }, false);
+}
