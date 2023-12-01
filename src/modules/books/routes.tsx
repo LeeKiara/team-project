3584631d8e41901a507dca92651d96a4d57fcf4f
Aejin Kang <dowlsdl003@naver.com>
2023-12-01 16:40:14 +0900

    싱글페이지 주소 수정

--- src/modules/books/routes.tsx
+++ src/modules/books/routes.tsx
@@ -3,22 +3,24 @@ import { lazy } from "react";
 import BookSearch from "./BookSearch";
 import BookPage from "./BookPage";
 import SlideBanner from "@/components/SlideBanner";
+import { isLocalhost } from "./data";
 
 const BookSidebar = lazy(() => import("./BookSidebar"));
 const BookList = lazy(() => import("./BookList"));
 const BookNewList = lazy(() => import("./BookNewList"));
 const BookBestList = lazy(() => import("./BookBestList"));
+const serverAddress = isLocalhost();
 
 export const booksRouts = [
   <Route key="books" path="books" element={<BookSidebar />}>
     ,
-    <Route element={<BookList fetchUrl="http://localhost:8081/books/paging/search?option=국내도서" />} index />,
+    <Route element={<BookList fetchUrl={`${serverAddress}/books/paging/search?option=국내도서`} />} index />,
     <Route key="best" path="best" element={<BookBestList />} />,
     <Route key="new" path="new" element={<BookNewList />} />,
     <Route
       key="foregin"
       path="foreign"
-      element={<BookList fetchUrl="http://localhost:8081/books/paging/search?option=외국도서" />}
+      element={<BookList fetchUrl={`${serverAddress}/books/paging/search?option=외국도서`} />}
     />
     ,
   </Route>,
