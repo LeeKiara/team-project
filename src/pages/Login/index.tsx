3b771522f17a3c367b35c4fbb9fc1c9ca7fe0ba9
Aejin Kang <dowlsdl003@naver.com>
2023-12-08 15:43:43 +0900

    아이디저장 오타 수정/로그인시 빈값 확인 수정 및 로그인 수동제출로 변경

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import { AccountCircle, CheckCircle, CheckCircleOutline, Lock } from "@mui/icons-material";
 import { LoginCantailner } from "./styles";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, MutableRefObject } from "react";
 import { Link } from "react-router-dom";
 import home_icon from "../../assets/homepage-icon.png";
 import { isLocalhost } from "@/modules/books/data";
@@ -10,6 +10,8 @@ const Login = () => {
   const [checkbox, setCheckbox] = useState(false);
   const serverAddress = isLocalhost();
   const [loginAddress, setLoginAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const formRef = useRef(null);
 
   const handleCheckbox = () => {
     if (checkbox) {
@@ -20,8 +22,34 @@ const Login = () => {
     setCheckbox(!checkbox);
   };
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (formRef !== null && userid !== "" && password !== "") {
+      formRef.current.submit();
+    }
+  };
+
+  const handleLoginClick = (e) => {
+    if (userid === "" && password === "") {
+      alert("아이디와 비밀번호를 입력해주세요.");
+    } else if (userid === "" && password !== "") {
+      alert("아이디를 입력해주세요.");
+    } else if (userid !== "" && password === "") {
+      alert("비밀 번호를 입력해주세요");
+    }
+    handleLogin(e);
+  };
+
+  const handleKeyUp = (e) => {
+    // Enter 키 눌렀을 때 로그인 처리
+    if (e.key === "Enter") {
+      handleLoginClick(e);
+    }
+  };
+
   useEffect(() => {
-    const savedUserId = localStorage.getItem("userId");
+    const savedUserId = localStorage.getItem("userid");
+    console.log(savedUserId);
     if (savedUserId) {
       setUserid(savedUserId);
       setCheckbox(true);
@@ -39,7 +67,7 @@ const Login = () => {
           <Link to="/">
             <img src={home_icon} alt="home icon" height={80} />
           </Link>
-          <form action={loginAddress} method="post">
+          <form ref={formRef} action={loginAddress} onSubmit={handleLogin} method="post">
             <label>
               <AccountCircle className="material-icons-outlined" />
               <input
@@ -52,9 +80,17 @@ const Login = () => {
             </label>
             <label>
               <Lock className="material-icons-outlined" />
-              <input type="password" name="password" placeholder="비밀번호를 입력하세요" />
+              <input
+                type="password"
+                value={password}
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호를 입력하세요"
+              />
             </label>
-            <button>로그인</button>
+            <button onClick={handleLoginClick} onKeyUp={handleKeyUp}>
+              로그인
+            </button>
             <span onClick={handleCheckbox}>
               {checkbox ? (
                 <CheckCircle className="material-icons-outlined" />
